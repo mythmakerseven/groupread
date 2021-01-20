6897ec833aa56cc54c461f077ee053d03dbcce61
Camden Mecklem <c@camdenmecklem.com>
2021-01-20 00:17:43 -0500

    revert previous commit

--- index.js
+++ index.js
@@ -7,10 +7,21 @@ const path = require('path')
 // backend handles all requests starting with /api
 const server = http.createServer(app)
 
-// other requests are routed to the /build folder in which the compiled frontend resides
+// other requests are routed to the /build folder
+// in which the compiled frontend resides
+app.get('/main.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'main.js'))
+})
+
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'favicon.ico'))
+})
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
+
+// TODO: this is not a good solution, we can't have a separate route for each file in /build
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
