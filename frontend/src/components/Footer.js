50bb0652bbd0a189029ebab00b1bb68dcf0b1252
Camden Mecklem <c@camdenmecklem.com>
2021-02-11 17:23:40 -0500

    remove extraneous copyright notice

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,6 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <div className='content has-text-centered'>
-        <p>&copy;2021 Camden Mecklem</p>
         <p>book metadata provided by the <a href="https://openlibrary.org/">Open Library API</a></p>
         <a href='https://www.youtube.com/watch?v=oHg5SJYRHA0'>Privacy Policy</a>
       </div>
