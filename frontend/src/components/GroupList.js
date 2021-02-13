dd45b778e4ebc80b3a24209f4e2a0c8aee3f8d52
Camden Mecklem <c@camdenmecklem.com>
2021-02-13 03:52:25 -0500

    slightly prettier group list

--- frontend/src/components/GroupList.js
+++ frontend/src/components/GroupList.js
@@ -18,15 +18,25 @@ const GroupList = () => {
         Group Read
       </h1>
       <p>Note: This list is temporary for development purposes. Version 1.0 will not have a public list of groups.</p>
-      <ul>
-        {groups.map(group =>
-          <li key={group.id}>
-            <Link to={`/groups/${group.id}`}>
-              {group.id} - {group.bookTitle}
-            </Link>
-          </li>
-        )}
-      </ul>
+      <div className='columns is-mobile is-centered'>
+        <div className='column is-5'>
+          <div className='list mt-4'>
+            <ul>
+              {groups.map(group =>
+                <div key={group.id} className='list-item'>
+                  <li className='has-text-centered mt-3 mb-3'>
+                    <Link to={`/groups/${group.id}`}>
+                      <strong className='is-size-4'>{group.bookTitle} by {group.bookAuthor}</strong>
+                      <br />
+                      (id: {group.id})
+                    </Link>
+                  </li>
+                </div>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
