eb7892754419129250a7c22bb64411bc95a02d8e
Camden Mecklem <c@camdenmecklem.com>
2021-02-17 21:29:48 -0500

    removed rickroll link from footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
     <footer className='footer'>
       <div className='content has-text-centered'>
         <p>book metadata provided by the <a href="https://openlibrary.org/">Open Library API</a></p>
-        <a href='https://www.youtube.com/watch?v=oHg5SJYRHA0'>Privacy Policy</a>
       </div>
     </footer>
   )
