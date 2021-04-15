1f33686f25e7c96434ce712b4b49929c9fe18281
Camden Mecklem <c@camdenmecklem.com>
2021-04-14 22:16:27 -0400

    add issues note to bottom

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,7 +4,8 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <div className='content has-text-centered'>
-        <p>book metadata provided by the <a href="https://openlibrary.org/">Open Library API</a></p>
+        <p>Book metadata provided by the <a href="https://openlibrary.org/">Open Library API</a></p>
+        <p>This website is currently under development. If you experience a bug, please <a href='https://github.com/mythmakerseven/groupread/issues'>report it</a>.</p>
       </div>
     </footer>
   )
