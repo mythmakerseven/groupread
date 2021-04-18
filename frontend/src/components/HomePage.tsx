52ad4220fe74675eb9b06616bf71882e6e20cd6e
Camden Mecklem <c@camdenmecklem.com>
2021-04-17 23:50:35 -0400

    improve homepage design

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -7,17 +7,20 @@ const HomePage = () => {
     <div>
       <section className='homepage-hero hero is-black is-fullheight-with-navbar' style={{ backgroundImage: `url(${backgroundImg})` }}>
         <div className='hero-body'>
-          <div className='container'>
-            <div className='has-text-centered hero-text p-4'>
-              <h1 className='title is-size-1 homepage-title p-4'>Read books with your friends.</h1>
-              <div className='content p-4'>
+          <div className='container is-max-desktop'>
+            <div className='has-text-centered hero-text p-6 mb-4'>
+              <h1 className='title is-size-1 is-size-3-mobile homepage-title'>Groupread</h1>
+              <h1 className='subtitle is-size-4 is-size-6-mobile homepage-title'>Read books with your friends.</h1>
+            </div>
+            <div className='has-text-centered hero-text p-6'>
+              <div className='content'>
                 <p>Join exclusive, members-only reading groups</p>
                 <p>Automatically schedule discussions</p>
                 <p>Invite your friends or make new ones</p>
               </div>
             </div>
             <div className='has-text-centered hero-text p-4 mt-4'>
-              <Link to='/groups' className='is-size-4 p-4 homepage-group-link'>
+              <Link to='/groups' className='is-size-4 is-size-6-mobile homepage-group-link'>
                 <b>See what people are reading &#10132;</b>
               </Link>
             </div>
