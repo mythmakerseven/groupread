19381b9759f067726db8244cbbe65ee41916c7cd
Camden Mecklem <c@camdenmecklem.com>
2021-05-04 17:05:57 -0400

    fix broken footer test

--- frontend/src/components/Footer.cypress.tsx
+++ frontend/src/components/Footer.cypress.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react'
 import { mount } from '@cypress/react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Footer from './Footer'
 
 // simple one to make sure cypress is working
 it('Footer', () => {
-  mount(<Footer />)
+  mount(
+  <Router>
+    <Footer />
+  </Router>)
   cy.get('footer').contains('Book metadata provided by the')
 })
